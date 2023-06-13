5bcc7c7f602523ad7e0eafa327509fd9f7c80f35
NaraSakamoto <nara.sakamoto@gmail.com>
2023-06-13 11:07:14 -0300

    Criando conexão com API de Login

--- src/components/InputText/index.js
+++ src/components/InputText/index.js
@@ -4,7 +4,7 @@ const InputText = (props) => {
     return(
         <div className="input-text">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder} ></input>
+            <input name={props.name} placeholder={props.placeholder} onChange={props.onChange}></input>
         </div>
     )
 }
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,17 +1,39 @@
 import './Login.css';
 import InputText from '../InputText'
-import Button from '../Button'
+import { useState } from "react"
 
 function Login() {
+    const [formData, setFormData] = useState({
+        email: '', // required
+        password: '' // required
+    })
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(formData)
+        fetch('http://localhost:8080/login', {
+            method: 'POST',
+            headers: {'Content-Type' : 'application/json'},
+            body: JSON.stringify(formData)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data.user))
+    }
+
+    function handleChange(e) {
+        console.log(e.target.value)
+        setFormData({...formData, [e.target.name] : e.target.value})
+    }
+
     return (
         <section className="Login" name="Login">
             <header className="App-header">
                 <img src='/images/logo.png' alt='Logo da marca Algo mais'/>
             </header>
-            <form>
-                <InputText label="Email" placeholder="Digite seu email aqui" />
-                <InputText label="Senha" placeholder="Digite sua senha aqui" />
-                <Button>Entrar</Button>
+            <form onSubmit={e => handleSubmit(e)}>
+                <InputText name="email" label="Email" placeholder="Digite seu email aqui" onChange={e => handleChange(e)}/>
+                <InputText name="password" label="Senha" placeholder="Digite sua senha aqui" onChange={e => handleChange(e)}/>
+                <button className="login-btn" type="submit">Entrar</button>
             </form>
         </section>
     );
